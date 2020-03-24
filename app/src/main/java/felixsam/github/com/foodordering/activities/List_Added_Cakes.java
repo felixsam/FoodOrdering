216bd17b4058ffa14a9da6092e60ba70cfc97071
Felix <felixsam94@gmail.com>
2020-03-23 23:52:52 -0700

    refactor list_added_cakes

--- app/src/main/java/felixsam/github/com/foodordering/activities/List_Added_Cakes.java
+++ app/src/main/java/felixsam/github/com/foodordering/activities/List_Added_Cakes.java
@@ -17,6 +17,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import felixsam.github.com.foodordering.DatabaseHelper;
 import felixsam.github.com.foodordering.Models.ItemModel;
@@ -25,21 +26,23 @@ import felixsam.github.com.foodordering.adapters.CustomAdapter_ItemList;
 
 
 public class List_Added_Cakes extends AppCompatActivity {
-    DatabaseHelper myDB;
-    ArrayList<ItemModel> cakeList;
-    ListView listview_cakes;
-    ItemModel cakes;
-    Button btn_ok, btn_cancel, btn_delete;
-    Dialog editCakeDialog;
+    private DatabaseHelper myDB;
+    private ArrayList<ItemModel> cakeList;
+    private ListView listview_cakes;
+    private ItemModel cakes;
+    private Button btn_ok;
+    private Button btn_cancel;
+    private Button btn_delete;
+    private Dialog editCakeDialog;
 
-    private String TAG = "ListCakes";
+    private final String TAG = "ListCakes";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_added_items);
         this.setTitle("List of Cake Orders");
-        TextView tv_cakename = (TextView) findViewById(R.id.item_column_header1);
+        TextView tv_cakename = findViewById(R.id.item_column_header1);
         tv_cakename.setText("Cake Name");
 
         myDB = new DatabaseHelper(this);
@@ -54,24 +57,24 @@ public class List_Added_Cakes extends AppCompatActivity {
             int i=0;
             while(data.moveToNext()){
                 //cakes = new ItemModel(data.getString(1),data.getString(2),data.getString(3));
-                cakes = new ItemModel(data.getColumnIndex(myDB.ITEMS_COL1_ID),data.getString(data.getColumnIndex(myDB.ITEMS_COL3_FIRST_NAME)),
-                        data.getString(data.getColumnIndex(myDB.ITEMS_COL5_ITEM_NAME)),
-                        data.getInt(data.getColumnIndex(myDB.ITEMS_COL6_PRICE)),
-                        data.getInt(data.getColumnIndex(myDB.ITEMS_COL7_QUANTITY)),
-                        data.getInt(data.getColumnIndex(myDB.ITEMS_COL2_USER_ID))
+                cakes = new ItemModel(data.getColumnIndex(DatabaseHelper.ITEMS_COL1_ID),data.getString(data.getColumnIndex(DatabaseHelper.ITEMS_COL3_FIRST_NAME)),
+                        data.getString(data.getColumnIndex(DatabaseHelper.ITEMS_COL5_ITEM_NAME)),
+                        data.getInt(data.getColumnIndex(DatabaseHelper.ITEMS_COL6_PRICE)),
+                        data.getInt(data.getColumnIndex(DatabaseHelper.ITEMS_COL7_QUANTITY)),
+                        data.getInt(data.getColumnIndex(DatabaseHelper.ITEMS_COL2_USER_ID))
                 );
                 cakeList.add(i, cakes);
-                System.out.println(data.getString(data.getColumnIndex(myDB.ITEMS_COL3_FIRST_NAME))+" "
-                        +data.getString(data.getColumnIndex(myDB.ITEMS_COL5_ITEM_NAME)) +" "
-                        +data.getInt(data.getColumnIndex(myDB.ITEMS_COL6_PRICE)) + " "
-                        +data.getInt(data.getColumnIndex(myDB.ITEMS_COL7_QUANTITY)) + " " +
-                        +data.getInt(data.getColumnIndex(myDB.ITEMS_COL2_USER_ID))
+                System.out.println(data.getString(data.getColumnIndex(DatabaseHelper.ITEMS_COL3_FIRST_NAME))+" "
+                        +data.getString(data.getColumnIndex(DatabaseHelper.ITEMS_COL5_ITEM_NAME)) +" "
+                        +data.getInt(data.getColumnIndex(DatabaseHelper.ITEMS_COL6_PRICE)) + " "
+                        +data.getInt(data.getColumnIndex(DatabaseHelper.ITEMS_COL7_QUANTITY)) + " " +
+                        +data.getInt(data.getColumnIndex(DatabaseHelper.ITEMS_COL2_USER_ID))
                 );
                 System.out.println(cakeList.get(i).getName());
                 i++;
             }
             CustomAdapter_ItemList adapter2 =  new CustomAdapter_ItemList(this,R.layout.adapter_item_list_columns, cakeList);
-            listview_cakes = (ListView) findViewById(R.id.drink_listView);
+            listview_cakes = findViewById(R.id.drink_listView);
             listview_cakes.setAdapter(adapter2);
 
             listview_cakes.setOnItemClickListener(new AdapterView.OnItemClickListener()
@@ -87,7 +90,7 @@ public class List_Added_Cakes extends AppCompatActivity {
 
                     editCakeDialog = new Dialog(List_Added_Cakes.this);
                     LayoutInflater customInflater = (LayoutInflater)List_Added_Cakes.this.getSystemService(LAYOUT_INFLATER_SERVICE);
-                    View customLayout=customInflater.inflate(R.layout.custom_dialog_edit_item, (ViewGroup) findViewById(R.id.root));
+                    View customLayout= Objects.requireNonNull(customInflater).inflate(R.layout.custom_dialog_edit_item, (ViewGroup) findViewById(R.id.root));
                     editCakeDialog.setContentView(customLayout);
                     ViewGroup.LayoutParams layoutParams2= customLayout.getLayoutParams();
                     layoutParams2.height=900;
@@ -95,9 +98,9 @@ public class List_Added_Cakes extends AppCompatActivity {
 
 
 
-                    final TextView tv_quantity = (TextView) customLayout.findViewById(R.id.custom_dialog_tv_quantity);
-                    TextView tv_item_name = (TextView) customLayout.findViewById(R.id.custom_dialog_tv_item_name);
-                    TextView tv_item_price = (TextView) customLayout.findViewById(R.id.custom_dialog_tv_item_price);
+                    final TextView tv_quantity = customLayout.findViewById(R.id.custom_dialog_tv_quantity);
+                    TextView tv_item_name = customLayout.findViewById(R.id.custom_dialog_tv_item_name);
+                    TextView tv_item_price = customLayout.findViewById(R.id.custom_dialog_tv_item_price);
 
                     Integer current_quantity = cakeList.get(position).getQuantity();
                     final String item_name = cakeList.get(position).getName();
@@ -108,7 +111,7 @@ public class List_Added_Cakes extends AppCompatActivity {
                     tv_item_price.setText("Price: $" + item_price.toString());
                     tv_quantity.setText(current_quantity.toString());
 
-                    btn_ok = (Button) customLayout.findViewById(R.id.btn_update_entry);
+                    btn_ok = customLayout.findViewById(R.id.btn_update_entry);
                     btn_ok.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
@@ -121,7 +124,7 @@ public class List_Added_Cakes extends AppCompatActivity {
                         }
                     });
 
-                    btn_delete = (Button)customLayout.findViewById(R.id.custom_dialog_delete);
+                    btn_delete = customLayout.findViewById(R.id.custom_dialog_delete);
                     btn_delete.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v){
@@ -132,7 +135,7 @@ public class List_Added_Cakes extends AppCompatActivity {
                         }
 
                     });
-                    btn_cancel =(Button)customLayout.findViewById(R.id.cancel);
+                    btn_cancel = customLayout.findViewById(R.id.cancel);
                     btn_cancel.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
