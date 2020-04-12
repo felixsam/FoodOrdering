7bc4e6340ff5961c83bc86c6c11ef7d218e71dee
Felix <felixsam94@gmail.com>
2020-04-11 23:10:19 -0700

    adding custom_dialog function to cocktail adapter

--- app/src/main/java/felixsam/github/com/foodordering/adapters/Adapter_Cocktail.java
+++ app/src/main/java/felixsam/github/com/foodordering/adapters/Adapter_Cocktail.java
@@ -1,5 +1,6 @@
 package felixsam.github.com.foodordering.adapters;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +15,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import felixsam.github.com.foodordering.Models.Cocktail;
 import felixsam.github.com.foodordering.R;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class Adapter_Cocktail extends RecyclerView.Adapter<Adapter_Cocktail.ViewHolder> {
     private String cocktail_name;
@@ -40,7 +44,7 @@ public class Adapter_Cocktail extends RecyclerView.Adapter<Adapter_Cocktail.View
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
 
         // this method will bind the data to the ViewHolder from whence it'll be shown to other Views
 
@@ -59,12 +63,34 @@ public class Adapter_Cocktail extends RecyclerView.Adapter<Adapter_Cocktail.View
 
         Glide.with(context).load(cocktail.getImage_url()).into(holder.iv_cocktail_thumb);
 
+
+
+
         holder.cv_cocktail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, cocktail.getCocktail_name() + "\n Instructions: \n"
                         + cocktail.getInstructions()
                         + "\n Ingredients: " + cocktail.getIngredients_single_string(), Toast.LENGTH_LONG).show();
+
+
+                LayoutInflater customInflater = (LayoutInflater)context.getSystemService(LAYOUT_INFLATER_SERVICE);
+                Dialog cocktail_dialog = new Dialog(context);
+
+                View customLayout = Objects.requireNonNull(customInflater.inflate(R.layout.custom_dialog_cocktail, holder.cocktail_dialog_root));
+                cocktail_dialog.setContentView(customLayout);
+                ViewGroup.LayoutParams layoutParams2= customLayout.getLayoutParams();
+                layoutParams2.height=900;
+                layoutParams2.width=900;
+
+                final TextView tv_cocktail_name = customLayout.findViewById(R.id.tv_cocktail_name);
+                tv_cocktail_name.setText(cocktail.getCocktail_name());
+
+
+
+                cocktail_dialog.show();
+
+
             }
         });
 
@@ -85,16 +111,19 @@ public class Adapter_Cocktail extends RecyclerView.Adapter<Adapter_Cocktail.View
         TextView tv_cocktail_glass;
         ImageView iv_cocktail_thumb;
         CardView cv_cocktail;
+        ViewGroup cocktail_dialog_root;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             // initialize the View objects
+            Dialog cocktail_dialog = new Dialog(context);
             this.cv_cocktail = itemView.findViewById(R.id.cv_cocktail_item);
             this.tv_cocktail_name = itemView.findViewById(R.id.tv_cocktail_name);
             this.tv_cocktail_id = itemView.findViewById(R.id.tv_cocktail_id);
             this.tv_cocktail_glass = itemView.findViewById(R.id.tv_cocktail_glass);
             this.iv_cocktail_thumb = itemView.findViewById(R.id.iv_cocktail_thumb);
+            this.cocktail_dialog_root = itemView.findViewById(R.id.dialog_cocktail_root);
 
         }
 
