f1cd762bf12e49d46e0ec7e0e48e70a2eb1482ca
Felix <felixsam@users.noreply.github.com>
2020-04-26 19:42:29 -0700

    add a drawable to the close imagebutton for editing item entry

--- app/src/main/java/felixsam/github/com/foodordering/DialogFragment_Edit_ItemEntry.java
+++ app/src/main/java/felixsam/github/com/foodordering/DialogFragment_Edit_ItemEntry.java
@@ -1,16 +1,21 @@
 package felixsam.github.com.foodordering;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
 
 public class DialogFragment_Edit_ItemEntry extends DialogFragment {
 
@@ -18,11 +23,19 @@ public class DialogFragment_Edit_ItemEntry extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.dialog_edit_item_fullscreen, container, false);
         final DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
-
         final String item_name = this.getArguments().getString("ITEM_NAME");
         Integer item_price = this.getArguments().getInt("ITEM_PRICE");
         final Integer col_id = this.getArguments().getInt("COL_ID");
         Integer item_quantity = this.getArguments().getInt("QUANTITY");
+
+        Drawable drawable_close = MaterialDrawableBuilder.with(getActivity())
+                .setIcon(MaterialDrawableBuilder.IconValue.CLOSE)
+                .setColor(Color.WHITE)
+                .setToActionbarSize()
+                .build();
+
+        ImageButton btn_close = rootView.findViewById(R.id.full_screen_dialog_btn_close);
+        btn_close.setImageDrawable(drawable_close);
 
         TextInputEditText et_item_name = rootView.findViewById(R.id.fullscreen_dialog_edit_item_name);
         final TextInputEditText et_item_quantity = rootView.findViewById(R.id.fullscreen_dialog_edit_item_quantity);
