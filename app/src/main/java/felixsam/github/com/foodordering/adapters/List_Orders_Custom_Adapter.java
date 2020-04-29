78c0ff867557615cd9139f312eab9703ae19b484
Felix <felixsam@users.noreply.github.com>
2020-04-28 23:49:56 -0700

    extend RecyclerView.Adapter onto order custom adapter

--- app/src/main/java/felixsam/github/com/foodordering/adapters/List_Orders_Custom_Adapter.java
+++ app/src/main/java/felixsam/github/com/foodordering/adapters/List_Orders_Custom_Adapter.java
@@ -5,7 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
 
 import java.util.ArrayList;
 
@@ -13,7 +18,7 @@ import felixsam.github.com.foodordering.DatabaseHelper;
 import felixsam.github.com.foodordering.Models.Order;
 import felixsam.github.com.foodordering.R;
 
-public class List_Orders_Custom_Adapter extends BaseAdapter {
+public class List_Orders_Custom_Adapter extends RecyclerView.Adapter<List_Orders_Custom_Adapter.MyViewHolder> {
 
     private Context context;
     private DatabaseHelper dbHelper;
@@ -21,80 +26,47 @@ public class List_Orders_Custom_Adapter extends BaseAdapter {
     private int mViewResourceId;
     private LayoutInflater mInflater;
 
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView total_quantity;
+        TextView item_name;
+        TextView total_item_price;
+        ImageView imgView;
 
-    public List_Orders_Custom_Adapter(Context context,int textViewResourceId, ArrayList<Order> OrderList){
-        this.context = context;
-        this.OrderList = OrderList;
-        this.dbHelper = new DatabaseHelper(context.getApplicationContext());
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mViewResourceId = textViewResourceId;
+        public MyViewHolder(View itemView){
+            super(itemView);
 
-
-    }
-    @Override
-    public int getViewTypeCount() {
-        return getCount();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-
-        return position;
+            this.total_quantity = itemView.findViewById(R.id.adapter_order_single_text_total_quantity);
+            this.item_name = itemView.findViewById(R.id.adapter_order_single_text_item_name);
+            this.total_item_price = itemView.findViewById(R.id.adapter_order_single_text_total_price);
+            this.imgView = itemView.findViewById(R.id.adapter_order_single_item_image);
+        }
     }
 
 
-
-    //TEST
     @Override
-    public int getCount() {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType){
+        View view =
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.adapter_order_single_item_card,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position){
+        holder.item_name.setText(OrderList.get(position).get_item_name());
+        holder.total_item_price.setText(OrderList.get(position).get_totalPrice().toString());
+        holder.total_quantity.setText(OrderList.get(position).getQuantity().toString());
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
         return OrderList.size();
     }
 
-    @Override
-    public Object getItem(int position) {
-        return OrderList.get(position);
-    }
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-
-    private class ViewHolder{
-        private TextView tv_user_name, tv_drinks, tv_total_price;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        convertView = mInflater.inflate(mViewResourceId, null);
-        ;
-        Order order = OrderList.get(position);
-
-        if (order != null) {
-            TextView total_quantity = (TextView) convertView.findViewById(R.id.order_total_quantity);
-            //TextView customer_name = (TextView) convertView.findViewById(R.id.order_user_name);
-            TextView item_name = (TextView) convertView.findViewById(R.id.order_item_name);
-            TextView total_item_price = (TextView) convertView.findViewById(R.id.order_item_total_price);
-            TextView item_price = (TextView) convertView.findViewById(R.id.order_item_price);
-
-            //TextView order_date = (TextView) convertView.findViewById(R.id.order_date);
-
-            if (total_quantity != null){
-                total_quantity.setText("Total Quantity: " + order.getQuantity().toString());
-            }
-            if (item_name != null){
-                item_name.setText("Item Name: "+ (order.get_item_name()));
-            }
-            if (total_item_price != null){
-                total_item_price.setText("Combined Price: $" + (order.get_totalPrice().toString()));
-            }
-            if (item_price != null){
-                item_price.setText("Item Price: $" + order.getPrice().toString());
-            }
-
-        }
-
-        return convertView;
-    }
 }
