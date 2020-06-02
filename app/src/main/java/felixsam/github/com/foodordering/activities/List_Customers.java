9e3a7198c658e06b05d7a64020d5780fce6bd891
Felix <felixsam@users.noreply.github.com>
2020-06-01 23:47:37 -0700

    refactor List_Customers

--- app/src/main/java/felixsam/github/com/foodordering/activities/List_Customers.java
+++ app/src/main/java/felixsam/github/com/foodordering/activities/List_Customers.java
@@ -17,8 +17,7 @@ import felixsam.github.com.foodordering.R;
 import felixsam.github.com.foodordering.adapters.List_Customers_Custom_Adapter;
 
 public class List_Customers extends AppCompatActivity {
-    //For logging
-    private static final String TAG = "List_Customers";
+    private static final String TAG = List_Customers.class.getSimpleName();
 
     private DatabaseHelper mDatabaseHelper;
     private Customer customer;
@@ -28,29 +27,28 @@ public class List_Customers extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_customers);
-        ListView mListView = findViewById(R.id.lv_customers);
-        mDatabaseHelper = new DatabaseHelper(this);
         this.setTitle("List of Customers");
-        Log.d(TAG, "populateListView: Displaying data in the ListView.");
+        ListView mListView = findViewById(R.id.lv_customers);
+        setContentView(R.layout.list_customers);
 
+        mDatabaseHelper = new DatabaseHelper(this);
+
+
+        Log.i(TAG, "Displaying data in the ListView.");
 
         //create the list adapter and set the adapter
-        ///ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         customer_list = new ArrayList<>();
         Cursor customer_data = mDatabaseHelper.getCustomer_ID_and_Name();
         int numRows = customer_data.getCount();
         if(numRows == 0){
-            Log.d(TAG,"Number of Rows is: " + numRows);
-            //Toast.makeText(List_Customers.this,"The Database is empty :( ", Toast.LENGTH_LONG).show();
+            Log.d(TAG,"No rows to populate list " + numRows);
         }else{
-            Log.d(TAG,"Hello");
-            Log.d(TAG,"Number of Rows is: " + numRows);
+            Log.i(TAG,"Number of Rows is: " + numRows);
 
             int i = 0;
             while(customer_data.moveToNext()){
-                System.out.println("New Entry");
-                System.out.println(customer_data.getInt(0)+" "+customer_data.getString(1));
+                Log.i(TAG,"New Row Entry: ");
+                Log.i(TAG,customer_data.getInt(0)+" "+customer_data.getString(1));
                 customer = new Customer(customer_data.getInt(0),customer_data.getString(1));
                 customer_list.add(i,customer);
                 i++;
@@ -58,15 +56,6 @@ public class List_Customers extends AppCompatActivity {
             adapter = new List_Customers_Custom_Adapter(this, customer_list);
             mListView.setAdapter(adapter);
         }
-
-        //populateListView();
     }
 
-    /**
-     * customizable toast
-     * @param message
-     */
-    private void toastMessage(String message){
-        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
-    }
 }
