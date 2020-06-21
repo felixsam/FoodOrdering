b21153191a0376f11be9394c11ff0866b23d1569
Felix <felixsam@users.noreply.github.com>
2020-06-20 23:22:22 -0700

    add LocationListener to MapActivity

--- app/src/main/java/felixsam/github/com/foodordering/activities/MapActivity.java
+++ app/src/main/java/felixsam/github/com/foodordering/activities/MapActivity.java
@@ -1,6 +1,10 @@
 package felixsam.github.com.foodordering.activities;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,17 +16,36 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import felixsam.github.com.foodordering.R;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
     private final String TAG = MapActivity.class.getSimpleName();
-
+    private Location location;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        boolean canGetLocation = false;
+        boolean isNetworkEnabled = false;
+        boolean isGPSEnabled = false;
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
         mapFragment.getMapAsync(this);
+
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // getting GPS status
+        isGPSEnabled = mLocationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // getting network status
+        isNetworkEnabled = mLocationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+
+        //mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000,10,this);
     }
 
     @Override
@@ -31,4 +54,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .position(new LatLng(0, 0))
                 .title("Marker"));
     }
+
+    @Override
+    public void onLocationChanged(Location location){
+        handleNewLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras){
+        Log.d(TAG,"Status changed : " + status);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider){
+        Log.d(TAG,"Provider Enabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider){
+        Log.d(TAG,"Provider Disabled");
+
+    }
+    private void handleNewLocation(Location location){
+        Log.d(TAG,location.toString());
+    }
+
+
 }
