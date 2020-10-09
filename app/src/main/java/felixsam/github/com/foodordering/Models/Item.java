076f0b393e3dc810cb31ec923bca3c4bc9fb5349
Felix <felixsam@users.noreply.github.com>
2020-10-08 22:41:26 -0700

    add new item model

---
+++ app/src/main/java/felixsam/github/com/foodordering/Models/Item.java
@@ -0,0 +1,75 @@
+package felixsam.github.com.foodordering.Models;
+
+public class Item {
+    private int itemId;
+    private int userId;
+    private String name;
+    private double price;
+    private int quantity;
+    private String category;
+    private int orderId;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+
+
+
+
+
+
+
+}
