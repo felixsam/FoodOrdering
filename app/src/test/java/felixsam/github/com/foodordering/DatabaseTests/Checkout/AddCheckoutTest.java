28586c6257a2eee31577356479b140d5f8d109ce
Felix <felixsam@users.noreply.github.com>
2021-01-17 22:36:31 -0800

    add test for checkout sql function

--- app/src/test/java/felixsam/github/com/foodordering/DatabaseTests/Checkout/AddCheckoutTest.java
+++ app/src/test/java/felixsam/github/com/foodordering/DatabaseTests/Checkout/AddCheckoutTest.java
@@ -11,13 +11,17 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 
 import felixsam.github.com.foodordering.DatabaseHelper;
 import felixsam.github.com.foodordering.Models.Checkout;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk={Build.VERSION_CODES.O_MR1})
@@ -56,6 +60,29 @@ public class AddCheckoutTest {
             System.out.println(actualCheckout.get(i));
         }
         assertEquals(expectedCheckout,actualCheckout);
+
+    }
+
+    @Test
+    public void checkoutItems(){
+        dbHelper.addItem(1,"One",12.34,3, "DRINKS");
+        dbHelper.addItem(1,"Two",12.34,3, "DRINKS");
+        dbHelper.addItem(1,"Three",12.34,3, "DRINKS");
+
+        //create an order
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        Integer orderID = dbHelper.getOrderID(1,currentDateTimeString);
+
+        dbHelper.checkoutItems(1,orderID);
+
+        int[] expectedResult = new int[]{orderID,orderID,orderID};
+        int[] actualResult = new int[3];
+
+        actualResult[0] = dbHelper.getOrderId(1);
+        actualResult[1] = dbHelper.getOrderId(2);
+        actualResult[2] = dbHelper.getOrderId(3);
+
+        assertTrue(Arrays.equals(expectedResult,actualResult));
 
     }
 
